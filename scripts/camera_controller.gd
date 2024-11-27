68d019ec67cdaddcb7d1e6d815a8a9ec8c7ab223
QualityHardwareStore <149911612+QualityHardwareStore@users.noreply.github.com>
2024-11-27 13:41:17 +0000

    cam controls
    cam zoom
    cam movement
    dither shader

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -1,6 +1,58 @@
+#extends Node3D
+#
+#@export var cam_rotation : int
+#
+#func _process(delta: float) -> void:
+	#rotation_degrees.y = cam_rotation
+
 extends Node3D
 
-@export var cam_rotation : int
+@onready var camRoot : Vector3 = self.position
+@onready var camYaw = $camYaw
+@onready var camPitch = $camYaw/camPitch
+@onready var camera = $camYaw/camPitch/Camera3D
 
-func _process(delta: float) -> void:
-	rotation_degrees.y = cam_rotation
+var camVector : Vector3
+
+var yaw : float = 0
+var pitch : float = 0
+
+var yaw_sensitivity : float = 0.15
+var pitch_sensitivity : float = 0.15
+
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+
+var mouse_look = false
+
+func _ready() -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("move_camera"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		mouse_look = true
+	
+	if event.is_action_released("move_camera"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		mouse_look = false
+	
+	if event is InputEventMouseMotion and mouse_look == true:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += -event.relative.y * pitch_sensitivity
+	
+	if event.is_action_pressed("zoom_in"):
+		camera.position += (camVector)/10
+	
+	if event.is_action_pressed("zoom_out"):
+		camera.position -= (camVector)/10
+	
+
+
+func _physics_process(delta: float) -> void:
+	camYaw.rotation_degrees.y = yaw
+	camPitch.rotation_degrees.x = pitch
+	
+	camVector = (camRoot - camera.position)
+	print(camVector)
+	#Global.forwardDirection = Vector2(position.x, position.z)
--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 4.0
+const SPEED = 5.0
 const JUMP_VELOCITY = 10
 
 @onready var tatu_model = $tatu
@@ -17,11 +17,15 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = -(direction.rotated(Vector3.UP, camera.rotation.y).normalized())
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -31,3 +35,6 @@ func _physics_process(delta: float) -> void:
 	
 	
 	move_and_slide()
+	
+	#debugging shit
+	
